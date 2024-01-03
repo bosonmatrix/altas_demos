9d9e192c71be5c2ac1b8f01f2b792fd61fa4e3cc
bosonmatrix <1451608870@qq.com>
2024-01-03 14:49:58 +0800

    parallel processing ver

--- SLACP_extract.py
+++ SLACP_extract.py
@@ -7,6 +7,7 @@ import pyproj
 import os
 from tqdm import tqdm
 import time
+from joblib import Parallel,delayed
 import warnings
  
 warnings.filterwarnings('ignore')
@@ -65,87 +66,59 @@ def numbering(points):
             ids.append(11)
     return set(ids)
 
-def extract_in_loop(cp_path,geosrs,prosrs,level_threshold,slope_threshold,base_points_arr=None,box=None,search_type=None,search_range=None):
-    '''
-    extracting adjacent points in iterations
-    input:
-    bp_file: base point(center of the circle) file
-    cp_path: control point file
-    geosrs: geolocation refrence system
-    prosrs: projection refrence system
-    level_threshold: the altimetry accuracy threshold, control point that is HIGHER than this will be excluded
-    slope_threshold: the slope threshold, control point whose slope is HIGHER than this will be excluded
-    search_type: 'range': search by range, 'amount': search by amount of adjacent points
-    search_range: search range, in meter
-    search_amount: amount of adjacent points
-    output:
-    nearby_cp_filtered: extracted adjacent points, further filtered under slope threshold and accuracy threshold
-    '''
-    filtered_indices=[]
-    # nearby_cp_filtered = pd.DataFrame(data=None,columns=['longitute','latitute','level','height level','terrain slope'])
-    # nearby_cp_filtered=np.array((len(base_points_arr)))
-    cp_dirs=os.listdir(cp_path)
+def adjacent_extract(cp_path,index,level_threshold,slope_threshold,search_range):
+    cp_dirs=os.listdir(SLACP_path)
+    nearby_cp_filtered = pd.DataFrame(columns=['bp_index', 'nearby_cp'])
+    tqdm.write(f'processing the {index+1}th base point...')
+    time.sleep(0.1)
+    nearby_cp_tmp=np.empty(shape=[0,4])
+    for cp_dir in cp_dirs:
+        if os.path.isdir(cp_path+cp_dir):
+            cp_sub_dir=cp_path+cp_dir+'/data/'
+            tmp_split=str.split(cp_dir,sep='_')
+            if eval(tmp_split[0]) in numbering([base_points.loc[index,['lon','lat']].values]):
+                cp_files=os.listdir(cp_sub_dir)
+                for cp_file in cp_files:
+                    tqdm.write(f'searching in the control point file {cp_file}...')
+                    time.sleep(0.1)
+                    control_points=read_cp_file(cp_sub_dir+cp_file)
+                    if control_points['lon'].min()<base_points.loc[index,'lon']<control_points['lon'].max() and control_points['lat'].min()<base_points.loc[index,'lat']<control_points['lat'].max():
+                        xprj, yprj = pyproj.transform(p1, p2,x=control_points['lon'],y=control_points['lat'])
+                        control_points['lon_prjd']=xprj
+                        control_points['lat_prjd']=yprj
+                        control_points_tree=neighbors.KDTree(control_points[['lon_prjd','lat_prjd']].values)
+                        filtered_indices=[]
+                        nearby_cp_indices=control_points_tree.query_radius(base_points.loc[index,['lon_prjd','lat_prjd']].values.reshape(1,-1),r=search_range)
+                        nearby_cp_indices=np.concatenate(nearby_cp_indices)
+                        for index_ in nearby_cp_indices:
+                            if control_points.iloc[index_,4]<=level_threshold and np.fabs(control_points.iloc[index_,5])<=slope_threshold:
+                                filtered_indices.append(index_)
+                        if len(filtered_indices)!=0:
+                            nearby_cp_tmp=np.vstack(control_points.iloc[filtered_indices,1:-3].values)
+                        nearby_cp_filtered.loc[len(nearby_cp_filtered),['bp_index','nearby_cp']]=[index,nearby_cp_tmp]
+    return nearby_cp_filtered
 
-    if search_type=='adjacent':
-        base_points=pd.DataFrame(base_points_arr,columns=['pointID','lon','lat','level'])
-        p1 = pyproj.Proj(init=geosrs)
-        p2 = pyproj.Proj(init=prosrs)
-        xprj, yprj = pyproj.transform(p1, p2,x=base_points['lon'],y=base_points['lat'])
-        tqdm.write('base points are projected')
-        time.sleep(0.1)
-        base_points['lon_prjd']=xprj
-        base_points['lat_prjd']=yprj
-        
-        nearby_cp_filtered = pd.DataFrame(columns=['bp_index', 'nearby_cp'])
-        
-        for index in tqdm(range(base_points.shape[0]),desc='extracting adjacent control points'):
-            tqdm.write(f'processing the {index+1}th base point...')
-            time.sleep(0.1)
-            nearby_cp_tmp=np.empty(shape=[0,4])
-            for cp_dir in cp_dirs:
-                if os.path.isdir(cp_path+cp_dir):
-                    cp_sub_dir=cp_path+cp_dir+'/data/'
-                    tmp_split=str.split(cp_dir,sep='_')
-                    if eval(tmp_split[0]) in numbering([base_points.loc[index,['lon','lat']].values]):
-                        cp_files=os.listdir(cp_sub_dir)
-                        for cp_file in cp_files:
-                            tqdm.write(f'searching in the control point file {cp_file}...')
-                            time.sleep(0.1)
-                            control_points=read_cp_file(cp_sub_dir+cp_file)
-                            if control_points['lon'].min()<base_points.loc[index,'lon']<control_points['lon'].max() and control_points['lat'].min()<base_points.loc[index,'lat']<control_points['lat'].max():
-                                xprj, yprj = pyproj.transform(p1, p2,x=control_points['lon'],y=control_points['lat'])
-                                control_points['lon_prjd']=xprj
-                                control_points['lat_prjd']=yprj
-                                control_points_tree=neighbors.KDTree(control_points[['lon_prjd','lat_prjd']].values)
-                                filtered_indices=[]
-                                nearby_cp_indices=control_points_tree.query_radius(base_points.loc[index,['lon_prjd','lat_prjd']].values.reshape(1,-1),r=search_range)
-                                nearby_cp_indices=np.concatenate(nearby_cp_indices)
-                                for index_ in nearby_cp_indices:
-                                    if control_points.iloc[index_,4]<=level_threshold and np.fabs(control_points.iloc[index_,5])<=slope_threshold:
-                                        filtered_indices.append(index_)
-                                if len(filtered_indices)!=0:
-                                    nearby_cp_tmp=np.vstack(control_points.iloc[filtered_indices,1:-3].values)
-                                nearby_cp_filtered.loc[len(nearby_cp_filtered),['bp_index','nearby_cp']]=[index,nearby_cp_tmp]
-    elif search_type=='box':
-        nearby_cp_filtered=pd.DataFrame(columns=['n','lon','lat','h_interp','ac_level','terrain_slope'])
-        for cp_dir in cp_dirs:
-            if os.path.isdir(cp_path+cp_dir):
-                cp_sub_dir=cp_path+cp_dir+'/data/'
-                tmp_split=str.split(cp_dir,sep='_')
-                tmp_points=np.array([[box[0],box[1]],[box[2],box[3]]])
-                if eval(tmp_split[0]) in numbering(tmp_points):
-                    cp_files=os.listdir(cp_sub_dir)
-                    for cp_file in cp_files:
-                        tqdm.write(f'searching in the control point file {cp_file}...')
-                        time.sleep(0.1)
-                        control_points=read_cp_file(cp_sub_dir+cp_file)
-                        # if (control_points['lon'].min()<box[0]<control_points['lon'].max() and control_points['lat'].min()<box[1]<control_points['lat'].max()) or\
-                        #     (control_points['lon'].min()<box[2]<control_points['lon'].max() and control_points['lat'].min()<box[3]<control_points['lat'].max()):
-                        indices=(box[0]<=control_points['lon']) & (control_points['lon']<=box[2]) & \
-                        (box[1]<=control_points['lat']) & (control_points['lat']<=box[3]) & \
-                        (control_points['ac_level']<=level_threshold) & \
-                        (control_points['terrain_slope'] <= slope_threshold)
-                        nearby_cp_filtered=pd.concat([nearby_cp_filtered,control_points[indices]],axis=0)
+def box_extract(cp_path,level_threshold,slope_threshold,box=None):
+    cp_dirs=os.listdir(cp_path)
+    nearby_cp_filtered=pd.DataFrame(columns=['n','lon','lat','h_interp','ac_level','terrain_slope'])
+    for cp_dir in cp_dirs:
+        if os.path.isdir(cp_path+cp_dir):
+            cp_sub_dir=cp_path+cp_dir+'/data/'
+            tmp_split=str.split(cp_dir,sep='_')
+            tmp_points=np.array([[box[0],box[1]],[box[2],box[3]]])
+            if eval(tmp_split[0]) in numbering(tmp_points):
+                cp_files=os.listdir(cp_sub_dir)
+                for cp_file in cp_files:
+                    tqdm.write(f'searching in the control point file {cp_file}...')
+                    time.sleep(0.1)
+                    control_points=read_cp_file(cp_sub_dir+cp_file)
+                    # if (control_points['lon'].min()<box[0]<control_points['lon'].max() and control_points['lat'].min()<box[1]<control_points['lat'].max()) or\
+                    #     (control_points['lon'].min()<box[2]<control_points['lon'].max() and control_points['lat'].min()<box[3]<control_points['lat'].max()):
+                    indices=(box[0]<=control_points['lon']) & (control_points['lon']<=box[2]) & \
+                    (box[1]<=control_points['lat']) & (control_points['lat']<=box[3]) & \
+                    (control_points['ac_level']<=level_threshold) & \
+                    (control_points['terrain_slope'] <= slope_threshold)
+                    nearby_cp_filtered=pd.concat([nearby_cp_filtered,control_points[indices]],axis=0)                   
     return nearby_cp_filtered
 
 def output_cps(base_points,nearby_cp,output_file,output_type=None,method_type=None):
@@ -155,6 +128,7 @@ def output_cps(base_points,nearby_cp,output_file,output_type=None,method_type=No
                 amount=len(base_points)
                 f.write(str(amount)+'\n')
                 for index in tqdm(range(amount),desc='writing to file'):
+                    index=1
                     f.write('Begin\t%s\t%.4f\t%.4f\t%.4f\n' %(str(base_points[index,0]),base_points[index,0],base_points[index,1],base_points[index,2]))
                     nearby_cp_tmp=nearby_cp.loc[index,'nearby_cp']
                     for each in nearby_cp_tmp:
@@ -168,7 +142,7 @@ def output_cps(base_points,nearby_cp,output_file,output_type=None,method_type=No
             pass
 
 if __name__=='__main__':
-    bp_file=r'E:/phD_career/活儿/173/data/base_points/ObjResidual.txt'
+    bp_file=r'E:/phD_career/活儿/173/data/base_points/ObjResidual_less.txt'
     SLACP_path=r'E:/phD_career/活儿/173/data/ATLAS/2020/'
     output_file=r'E:/phD_career/活儿/173/data/base_points/ObjResidual_adjacent_cps.txt'
     
@@ -176,19 +150,25 @@ if __name__=='__main__':
     prosrs="epsg:32651"
     level_threshold=3
     slope_threshold=0.1
-    search_type='box'
-    search_range=500#5e5
+    search_range=500
     box=[117,37,119,45]
 
     base_points_arr=read_bp_file(bp_file)
-    extracted_points=extract_in_loop(SLACP_path,geosrs,prosrs,level_threshold,slope_threshold,base_points_arr=None,box=box,search_type=search_type,search_range=search_range)
-    # with open('nearby_cp_filtered_t.txt', 'r') as file:
-    #     content = file.read()
-    # # extracted_points_str = eval(content)
-    # extracted_points = []
-    # for sublist in content:
-    #     numbers = [float(elem) for elem in sublist]
-    #     extracted_points.append(numbers)
-    output_cps(base_points_arr,extracted_points,output_file,output_type='SAR',method_type='box')
+    base_points=pd.DataFrame(base_points_arr,columns=['pointID','lon','lat','level'])
+    p1 = pyproj.Proj(init=geosrs)
+    p2 = pyproj.Proj(init=prosrs)
+    xprj, yprj = pyproj.transform(p1, p2,x=base_points['lon'],y=base_points['lat'])
+    tqdm.write('base points are projected')
+    time.sleep(0.1)
+    base_points['lon_prjd']=xprj
+    base_points['lat_prjd']=yprj
+
+    adjacent_cp_total = pd.DataFrame(columns=['bp_index', 'nearby_cp'])
+    adjacent_cp=Parallel(n_jobs=-1)(delayed(adjacent_extract)(SLACP_path,index,level_threshold,slope_threshold,search_range) for index in tqdm(range(base_points.shape[0]),desc='extracting'))
+    
+    # adjacent_cp=adjacent_extract(SLACP_path,0,level_threshold,slope_threshold,search_range)
+    for each in adjacent_cp:
+        adjacent_cp_total=pd.concat([adjacent_cp_total,each],axis=0,ignore_index=True)
+    output_cps(base_points_arr,adjacent_cp_total,output_file,output_type='SAR',method_type='adjacent')
 
     tqdm.write('done')
